785d69a201d6def263d37c72f75ffddac2fbbacb
Pathak_ofc <bimalpathak6667@gmail.com>
2026-05-11 16:19:25 +0545

    send message done

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import { chatStore } from "../store/message.Store";
+import { ImageIcon, SendIcon, XIcon } from "lucide-react";
 
 function MessageInput() {
+  const { sendMessage } = chatStore();
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    if (!text.trim() && !imagePreview) return;
+
+    console.log({ text, imagePreview });
+    sendMessage({
+      text: text.trim(),
+      image: imagePreview,
+    });
+
+    setText("");
+    setImagePreview("");
+    if (fileInputRef.current) fileInputRef.current.value = "";
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (!file.type.startsWith("image/")) {
+      toast.error("Please select an image file");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onloadend = () => setImagePreview(reader.result);
+    reader.readAsDataURL(file);
+  };
+
+  const removeImage = () => {
+    setImagePreview(null);
+    if (fileInputRef.current) fileInputRef.current.value = "";
+  };
+
   return (
-    <div>MessageInput</div>
-  )
+    <div className="p-4 border-t border-slate-700/50">
+      {imagePreview && (
+        <div className="flex items-center max-w-3xl mx-auto mb-3">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="object-cover w-20 h-20 border rounded-lg border-slate-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute flex items-center justify-center w-6 h-6 rounded-full -top-2 -right-2 bg-slate-800 text-slate-200 hover:bg-slate-700"
+              type="button"
+            >
+              <XIcon className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
+      )}
+
+      <form
+        onSubmit={handleSendMessage}
+        className="flex max-w-3xl mx-auto space-x-4"
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          className="flex-1 px-4 py-2 border rounded-lg bg-slate-800/50 border-slate-700/50"
+          placeholder="Type your message..."
+        />
+
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleImageChange}
+          className="hidden"
+        />
+
+        <button
+          type="button"
+          onClick={() => fileInputRef.current?.click()}
+          className={`bg-slate-800/50 text-slate-400 hover:text-slate-200 rounded-lg px-4 transition-colors ${
+            imagePreview ? "text-cyan-500" : ""
+          }`}
+        >
+          <ImageIcon className="w-5 h-5" />
+        </button>
+        <button
+          type="submit"
+          disabled={!text.trim() && !imagePreview}
+          className="px-4 py-2 font-medium text-white transition-all rounded-lg bg-gradient-to-r from-cyan-500 to-cyan-600 hover:from-cyan-600 hover:to-cyan-700 disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          <SendIcon className="w-5 h-5" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default MessageInput
+export default MessageInput;
